c4b4cd5fbd65c11241d34a35d92b518a2fb8d4b7
IvanSkrynnik <ivan.skrinnik.nomia@gmail.com>
2024-09-13 20:46:56 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
+
 function App() {
+
+  const [name, setName] = useState("");
+  const [notes, setNotes] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if(name.trim() !== ""){
+      setNotes([...notes,name]);
+      setName("");
+    }
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form onSubmit={handleSubmit}>
+ 	      <label>Write Note</label><br/>
+        
+        <input type="text" value={name} onChange={(e)=>setName(e.target.value)}></input>
+        
+        <br/>
+        <br/>
+        <input type='submit' value="Submit"></input>
+      </form>
+      <div className="list">
+
+        {notes.map((note, index) => (
+          <div key={index} className="note" onClick={()=>setNotes(notes.filter((_, i) => i !== index))}>
+            {note}
+          </div>
+        ))}
+
+      </div>
     </div>
   );
 }
